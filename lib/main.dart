29297287966236fa31c858e5d04e35f5f22da909
Harshit036 <harshit_v@ce.iitr.ac.in>
2021-08-08 12:30:45 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:yoga/asans/asan1.dart';
-import 'package:yoga/asans/asan2.dart';
-import 'package:yoga/asans/asan3.dart';
-import 'package:yoga/asans/asan4.dart';
-import 'package:yoga/asans/asan5.dart';
-import 'package:yoga/asans/asan6.dart';
-import 'package:yoga/homepage.dart';
+
 import 'package:yoga/loginpage.dart';
 
 void main() {
